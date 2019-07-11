062ce579a4935cad322e9872f55585f58df86522
Michael Mottola <mottola64@gmail.com>
2019-07-11 02:11:30 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,16 +11,14 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
-    @pet = Pet.create(params[:pet][:name])
-    if params[:pet][:owner_ids]
-      @pet.owner_id = params[:pet][:owner_ids].first
-    elsif params[:owner_name]
-      owner = Owner.create(name: params[:owner_name])
-      @pet.owner = owner
+      @pet = Pet.create(params[:pet])
+      if !params["owner"]["name"].empty?
+        @pet.owner = Owner.create(name: params[:owner][:name])
+        @pet.save
+      end
+
+      redirect to "pets/#{@pet.id}"
     end
-    @pet.save
-    redirect "pets/#{@pet.id}"
-  end
 
   get '/pets/:id' do
     @pet = Pet.find(params[:id])
